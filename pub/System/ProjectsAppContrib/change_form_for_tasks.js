675e0b89146c4f7ba2535644454f4a3899883f67
Daniel Supplieth <supplieth@modell-aachen.de>
2015-12-08 15:01:35 +0100

    Make JS compatible for different webs
    Ticket: #10261
    Release-Note: none

--- pub/System/ProjectsAppContrib/change_form_for_tasks.js
+++ pub/System/ProjectsAppContrib/change_form_for_tasks.js
@@ -1,13 +1,13 @@
 $('document').ready(function() {
    $('.tasktracker').on('beforeCreate', function(e,t) {
       if(t.parent){
-         t.form="Projects.TasksForm";
+         t.form= foswiki.preferences.WEB+".TasksForm";
          t.depth=0;
       }
    });
    $('.tasktracker').on('beforeSave', function(e,t) {
       if (t.Parent){
-         t.form="Projects.TasksForm";
+         t.form= foswiki.preferences.WEB+".TasksForm";
       }
    });
 });
