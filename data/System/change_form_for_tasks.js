7d05ea2d9f2dbdd5e4097a00302477281d5946eb
Daniel Supplieth <supplieth@modell-aachen.de>
2015-12-08 13:50:57 +0100

    Added Tabs to WebHome and better child tasks
    Release-Note: none
    Ticket: none

--- data/System/change_form_for_tasks.js
+++ data/System/change_form_for_tasks.js
@@ -1,9 +1,13 @@
 $('document').ready(function() {
    $('.tasktracker').on('beforeCreate', function(e,t) {
       if(t.parent){
-         console.log(t);
          t.form="Projects.TasksForm";
          t.depth=0;
+      }
+   });
+   $('.tasktracker').on('beforeSave', function(e,t) {
+      if (t.Parent){
+         t.form="Projects.TasksForm";
       }
    });
 });
