9b3c17167746236f3c67fb51c5d2a881dce588ff
Sven Meyer <meyer@modell-aachen.de>
2016-01-06 09:45:05 +0100

    Fixed ADDTOZONE dependencies, refactored JS

--- pub/System/ProjectsAppContrib/change_form_for_tasks.js
+++ pub/System/ProjectsAppContrib/change_form_for_tasks.js
@@ -1,13 +1,18 @@
-$('document').ready(function() {
-   $('.tasktracker').on('beforeCreate', function(e,t) {
-      if(t.parent){
-         t.form= foswiki.preferences.WEB+".TasksForm";
-         t.depth=0;
-      }
-   });
-   $('.tasktracker').on('beforeSave', function(e,t) {
-      if (t.Parent){
-         t.form= foswiki.preferences.WEB+".TasksForm";
-      }
-   });
+$(document).ready(function() {
+   $('.tasktracker')
+      .on('beforeCreate', function(e,t) {
+         if(t.parent){
+            t.form= foswiki.preferences.WEB+".TasksForm";
+            t.depth=0;
+         }
+
+         return t;
+      })
+      .on('beforeSave', function(e,t) {
+         if (t.Parent){
+            t.form= foswiki.preferences.WEB+".TasksForm";
+         }
+
+         return t;
+      });
 });
